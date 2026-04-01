82098aed0ab2a696885ab116fb23afee16b879c7
六个点 <735966171@qq.com>
2026-04-01 16:37:00 +0800

    入口

--- run_agent.py
+++ run_agent.py
@@ -1,6 +1,8 @@
 from src.sync_a2a.agent import run_sync_agent
 from src.stream_a2a.agent import run_stream_agent
+from src.push_a2a.agent import run_push_agent
 
 if __name__ == "__main__":
     # run_sync_agent()
-    run_stream_agent()
+    # run_stream_agent()
+    run_push_agent()
--- run_client.py
+++ run_client.py
@@ -1,7 +1,9 @@
 import asyncio
-# from src.sync_a2a.client import run_sync_client
+from src.sync_a2a.client import run_sync_client
 from src.stream_a2a.client import run_stream_client
+from src.push_a2a.client import run_push_client
 
 if __name__ == "__main__":
     # asyncio.run(run_sync_client())
-    asyncio.run(run_stream_client())
+    # asyncio.run(run_stream_client())
+    asyncio.run(run_push_client())
